02952830bc3ff8a23cc0f7939f17c2f3b425a511
Wolfgang Resch <wresch@mail.nih.gov>
2012-06-04 21:36:59 -0400

    fastq-to-phred33 now allows truncation of Q > 40.

--- gosr/tools/fastq_to_phred33.py
+++ gosr/tools/fastq_to_phred33.py
@@ -13,6 +13,10 @@ to translate old solexa to new solexa character strings
 
 phred64 is a simple left shift
 
+phred33+ makes sure that q values don't exceed 40 (as
+it does in recent illumina output; currently works
+for input up to Q45).
+
 Output goes to stdout
 """
 
@@ -35,11 +39,15 @@ phred64  = r"""@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\]^_`abcdefghijklmnopqrstuvwxyz{|}~""
 phred33  = r"""!"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\]^_"""
 phred64_to_phred33 = maketrans(phred64, phred33)
 
+phred33plus_to_phred33 = maketrans("JKLMN", "IIIII")
+
 def to_phred33(args):
     if args.score_type == "phred64":
         table = phred64_to_phred33
     elif args.score_type == "solexa":
         table = solexa_to_phred33
+    elif args.score_type == "phred33+":
+        table = phred33plus_to_phred33
     out = "@{0}\n{1}\n+\n{2}"
     with FileOrGzip(args.fastq) as infile:
         for rid, rseq, rqual in fastq.read(infile):
@@ -55,7 +63,7 @@ def setup(commands):
             help = """changes from solexa of phred 64 to phred33""",
             formatter_class = argparse.RawDescriptionHelpFormatter,
             description     = __doc__)
-    cmdline.add_argument("score_type", choices = ["phred64", "solexa"],
+    cmdline.add_argument("score_type", choices = ["phred64", "solexa", "phred33+"],
             help = "Current score type")
     cmdline.add_argument("fastq", type = arghelpers.infilename_check,
             help = "Fastq file (can be .gz); '-' reads from stdin")
